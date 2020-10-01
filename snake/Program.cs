1854bc67d80524f9ef688f45503d7c6cf417c55e
Alexander Dolgosheev <alexanderdolgosheev@gmail.com>
2020-10-01 14:42:33 +0600

    start dev

---
+++ snake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace snake
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Start dev snake!");
+            Console.Read();
+        }
+    }
+}
